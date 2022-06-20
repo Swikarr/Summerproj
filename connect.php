f09c829fade97883b530640ca064e6170db71f5e
Swikarr <emailaddr>
2022-06-20 14:07:18 +0545

    Sign up Validation complete

--- connect.php
+++ connect.php
@@ -13,17 +13,39 @@ else{
   @$email = $_POST['email'];
   @$psw = $_POST['psw'];
   @$rpsw= $_POST['rpsw'];
+
+  $pass=password_hash($psw, PASSWORD_BCRYPT);
+  $rpass=password_hash($rpsw,PASSWORD_BCRYPT);
+
+  $emailquery= "select * from registration where email='$email'";
+  $query=mysqli_query($conn,$emailquery);
+
+  $emailcount= mysqli_num_rows($query);
+
+  if($emailcount>0){
+   echo "Email already present";
+  }else{
+     if($psw ===$rpsw){
+
     $stmt = $conn->prepare("insert into registration(username,email,psw,rpsw) values(?,?,?,?)");
     $stmt->bind_param("ssss", $username , $email, $psw, $rpsw );
     $stmt->execute();
-    echo "Registration Successful!";
-    //Redirect
-  //  header("location: http://localhost/summerproj/");
     $stmt->close();
     $conn->close();
+    echo "Registration Successful!";
+     } 
+    else{
+      echo "The password doesn't match!" ."  ". "  ". "<a href='Signup.php'>Sign up Again </a>";
+     
+     }
+    //Redirect
+  //  header("location: http://localhost/summerproj/");
+
      exit();
+
+     }
+    }
   }
-}
     ?>
 
 
